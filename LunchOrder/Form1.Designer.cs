c00a33f16e0b7dfcc1616f698bfb0a71e9400038
Evan Ward <enjoievan@gmail.com>
2022-07-13 12:36:47 -0700

    Decided that was dumb so I made it small instead.

--- LunchOrder/Form1.Designer.cs
+++ LunchOrder/Form1.Designer.cs
@@ -198,7 +198,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(434, 242);
+            this.button1.Location = new System.Drawing.Point(412, 222);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(315, 194);
             this.button1.TabIndex = 14;
